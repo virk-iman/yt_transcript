e67b8ff1c03140b45216c28d902ad2572bbc005c
virk-iman <virk.iman@gmail.com>
2026-04-08 19:42:39 +0530

    jn

--- frontend/yt_transcript/src/main.jsx
+++ frontend/yt_transcript/src/main.jsx
@@ -3,14 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker registered.', reg))
-      .catch(err => console.error('Service Worker registration failed:', err));
-  });
-}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
