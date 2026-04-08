c01baf98dd7cc8c6579a7ad0fc146e0a2dbc9a73
virk-iman <virk.iman@gmail.com>
2026-04-08 19:47:17 +0530

    k

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ const summarizeQueue = new Queue('summarize', { connection });
 const summaryCache = new Map();
 
 app.use(cors({
-    origin: ['https://yt-transcript-sooty.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
